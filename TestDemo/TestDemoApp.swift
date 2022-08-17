59ab4acb61fc2c229c84fa8e9ada5414acb3ec06
Onofras Denis <onofras.denisd@gmail.com>
2022-08-17 11:53:59 +0300

    Initial Commit

---
+++ TestDemo/TestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDemoApp.swift
+//  TestDemo
+//
+//  Created by Denis Onofras on 17.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
